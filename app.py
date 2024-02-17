b32c2d47133ffd6d39b4e789c3f94454008c54c0
rr-h <reuther.hols@gmail.com>
2024-02-17 13:02:43 +0000

    pre-gemini

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
 
